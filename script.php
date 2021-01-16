e3ff2b093c54ab01d171f7e7f809ca7d40278c35
Denys <denys@joomla-ua.org>
2021-01-16 15:37:39 +0200

    Add package

---
+++ script.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * @package        JUImage
+ * @subpackage     Class
+ *
+ * @author         Denys D. Nosov (denys@joomla-ua.org)
+ * @copyright (C)  2011-2021 by Denys D. Nosov (https://joomla-ua.org)
+ * @license        GNU General Public License version 2 or later
+ *
+ * @since          3.0
+ */
+
+defined('_JEXEC') or die;
+
+class Pkg_JUImageInstallerScript
+{
+	/**
+	 * @since    3.0
+	 */
+	public function __construct()
+	{
+
+	}
+
+	/**
+	 * @param $type
+	 * @param $parent
+	 *
+	 * @since    3.0
+	 */
+	public function preflight($type, $parent)
+	{
+
+	}
+
+	/**
+	 * @param $parent
+	 *
+	 * @since    3.0
+	 */
+	public function uninstall($parent)
+	{
+
+	}
+
+	/**
+	 * @param $parent
+	 *
+	 * @since    3.0
+	 */
+	public function update($parent)
+	{
+
+	}
+
+	/**
+	 * @return bool
+	 * @since    3.0
+	 */
+	public function postflight()
+	{
+		$path = JPATH_SITE . '/libraries/juimage/';
+
+		$files = [
+			$path . 'Image.php',
+			$path . 'index.html'
+		];
+
+		$folders = [
+			$path . 'classes',
+			$path . 'vendor/rosell-dk/webp-convert/src/Converters/Binaries'
+		];
+
+		foreach( $files AS $file )
+		{
+			if( file_exists($file) )
+			{
+				unlink($file);
+			}
+		}
+
+		foreach( $folders AS $folder )
+		{
+			if( is_dir($folder) )
+			{
+				$this->unlinkRecursive($folder, 1);
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param $dir
+	 * @param $deleteRootToo
+	 *
+	 * @since    3.0
+	 */
+	public function unlinkRecursive($dir, $deleteRootToo)
+	{
+		if(!$dh = opendir($dir))
+		{
+			return;
+		}
+
+		while(false !== ($obj = readdir($dh)))
+		{
+			if($obj === '.' || $obj === '..')
+			{
+				continue;
+			}
+
+			if(!unlink($dir . '/' . $obj))
+			{
+				$this->unlinkRecursive($dir . '/' . $obj, true);
+			}
+		}
+
+		closedir($dh);
+
+		if($deleteRootToo)
+		{
+			rmdir($dir);
+		}
+	}
+}
