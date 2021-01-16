9882960de453543f2583ff424e86b87e100a703d
Denys <denys@joomla-ua.org>
2021-01-16 15:36:18 +0200

    Move libs

--- packages/lib_juimage/JUImage.php
+++ packages/lib_juimage/JUImage.php
@@ -4,7 +4,7 @@
  * @subpackage     Class
  *
  * @author         Denys D. Nosov (denys@joomla-ua.org)
- * @copyright (C)  2011-2020 by Denys D. Nosov (https://joomla-ua.org)
+ * @copyright (C)  2011-2021 by Denys D. Nosov (https://joomla-ua.org)
  * @license        GNU General Public License version 2 or later
  *
  * @since          3.0
--- packages/lib_juimage/script.php
+++ packages/lib_juimage/script.php
@@ -4,7 +4,7 @@
  * @subpackage     Class
  *
  * @author         Denys D. Nosov (denys@joomla-ua.org)
- * @copyright (C)  2016-2019 by Denys D. Nosov (https://joomla-ua.org)
+ * @copyright (C)  2016-2021 by Denys D. Nosov (https://joomla-ua.org)
  * @license        GNU General Public License version 2 or later
  *
  * @since          3.0
