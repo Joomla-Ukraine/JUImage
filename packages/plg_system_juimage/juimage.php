371f8ef9065797baa33c2bef8db701c15cbafd8b
Denys <denys@joomla-ua.org>
2021-01-16 15:34:50 +0200

    Add systen plugin

---
+++ packages/plg_system_juimage/juimage.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package        JUImage
+ * @subpackage     Class
+ *
+ * @author         Denys D. Nosov (denys@joomla-ua.org)
+ * @copyright (C)  2011-2021 by Denys D. Nosov (https://joomla-ua.org)
+ * @license        GNU General Public License version 2 or later
+ *
+ * @since          3.0
+ */
+
+use Joomla\CMS\Plugin\CMSPlugin;
+
+defined('_JEXEC') or die;
+
+/**
+ * JUImage plugin class.
+ *
+ * @package  JUImage plugin
+ *
+ * @since 3.0
+ */
+class plgSystemJUImage extends CMSPlugin
+{
+	/**
+	 * @since 3.0
+	 */
+	public function onAfterInitialise()
+	{
+		require_once JPATH_LIBRARIES . '/juimage/JUImage.php';
+	}
+}
